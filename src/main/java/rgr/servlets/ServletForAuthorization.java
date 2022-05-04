b127e86428ae675b65c90e2b709ca3b402728ee3
SaitovErik <saitoverik@mail.ru>
2022-05-04 22:47:48 +0400

    Внесение изменений

--- src/main/java/rgr/servlets/ServletForAuthorization.java
+++ src/main/java/rgr/servlets/ServletForAuthorization.java
@@ -11,11 +11,24 @@ import javax.servlet.http.HttpServletResponse;
 
 import rgr.sqlManager.SQLReader;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ServletForAuthorization.
+ */
 @WebServlet(name="ServletForAuthorization", urlPatterns="/AuthorizationServlet") 
 public class ServletForAuthorization extends HttpServlet{
 	
+	/** The password. */
 	private String login, password;
 	
+	/**
+	 * Do post.
+	 *
+	 * @param request the request
+	 * @param response the response
+	 * @throws ServletException the servlet exception
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
 			checkLogin(request, response);
@@ -27,16 +40,39 @@ public class ServletForAuthorization extends HttpServlet{
 	}
 
 	
+	/**
+	 * The Class RequestCalc.
+	 */
 	public static class RequestCalc {
 		
+		/** The login. */
 		private String login;
+		
+		/** The password. */
 		private String password;
 		
+		/**
+		 * Instantiates a new request calc.
+		 *
+		 * @param login the login
+		 * @param password the password
+		 * @throws ServletException the servlet exception
+		 * @throws IOException Signals that an I/O exception has occurred.
+		 */
 		private RequestCalc (String login, String password) throws ServletException, IOException {
 			this.login = login;
 			this.password = password;
 			}
 		
+		/**
+		 * From request parameters.
+		 *
+		 * @param request the request
+		 * @param response the response
+		 * @return the request calc
+		 * @throws ServletException the servlet exception
+		 * @throws IOException Signals that an I/O exception has occurred.
+		 */
 		public static RequestCalc fromRequestParameters(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			return new RequestCalc(
 			request.getParameter("log"),
@@ -44,16 +80,33 @@ public class ServletForAuthorization extends HttpServlet{
 			);
 			}
 		
+		/**
+		 * Gets the login.
+		 *
+		 * @return the login
+		 */
 		public String getLogin() {
 			return login;
 			
 		}
 		
+		/**
+		 * Gets the password.
+		 *
+		 * @return the password
+		 */
 		public String getPassword() {
 			return password;
 		}
 		}
 	
+	/**
+	 * Check login.
+	 *
+	 * @param request the request
+	 * @param response the response
+	 * @throws Exception the exception
+	 */
 	public void checkLogin(HttpServletRequest request, HttpServletResponse response) throws Exception  {
 		RequestCalc Calc = RequestCalc.fromRequestParameters(request, response);
 		login = Calc.getLogin();
