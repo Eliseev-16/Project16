ddb79cfb96c308d3a672f1f3d0b5e11e98e3ff2c
Eliseev <Linkwood-123_9@mail.ru>
2022-05-06 00:09:26 +0200

    - Добавление проверки корректности введенных при регистрации данных

--- src/main/java/rgr/sqlManager/SQLAdder.java
+++ src/main/java/rgr/sqlManager/SQLAdder.java
@@ -2,6 +2,8 @@ package rgr.sqlManager;
 
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -18,6 +20,18 @@ public class SQLAdder {
      * @throws Exception the exception
      */
     public static void addUser(String login, String password, String access) throws Exception {
+        String mask = "[A-Za-z0-9]";
+
+        Pattern pt = Pattern.compile(mask);
+        for (int i = 0; i < login.length(); i++) {
+            Matcher matcher = pt.matcher(Character.toString(login.charAt(i)));
+            if (!matcher.matches()) throw new Exception("Only numbers or English characters are allowed");
+        }
+        for (int i = 0; i < password.length(); i++) {
+            Matcher matcher = pt.matcher(Character.toString(password.charAt(i)));
+            if (!matcher.matches()) throw new Exception("Only numbers or English characters are allowed");
+        }
+
 
         try {
             SQLConnection conn = new SQLConnection();
